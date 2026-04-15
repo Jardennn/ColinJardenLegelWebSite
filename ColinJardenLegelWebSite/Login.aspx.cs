ae36c20b9434a6661c6646b04ec9031b04619d36
Jardennn <jarden.legel@gmail.com>
2026-04-16 00:11:07 +0300

    Added a feature that changes the picture on the top of the website according to the weekday and each day has it's own image, and added a login feature (currently doesn't really work).

---
+++ ColinJardenLegelWebSite/Login.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (IsPostBack)
+        {
+            if (Request.Form["userName"] == "test" && Request.Form["password"] == "1234")
+            {
+                Session["userName"] = Request.Form["userName"];
+                Session["isLoggedIn"] = true;
+                Response.Redirect("Default.aspx");
+            }
+            else
+            {
+                LoginResult.InnerText = " שםמשתמשאוסיסמהאינםנכונים ";
+            }
+        }
+        Session.Abandon();
+
+        Response.Redirect("Default.aspx");
+    }
+}
--- ColinJardenLegelWebSite/MasterPage.master.cs
+++ ColinJardenLegelWebSite/MasterPage.master.cs
@@ -9,6 +9,51 @@ public partial class MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+            // Display the current server date in the header
+            DateLabel.Text = DateTime.Now.ToString("d");
+            // Set the image based on the day of the week
+            string dayOfWeek = DateTime.Now.DayOfWeek.ToString();
+            string imagePath = GetImagePathForDay(dayOfWeek);
+            DayImage.ImageUrl = imagePath;
+            DayImage.AlternateText = $"Image for {dayOfWeek}";
 
+            if ((bool)Session["isLoggedIn"])
+            {
+                LoginLogout.HRef = "Logout.aspx";
+                LoginLogout.InnerText = "שלום, " + Session["userName"] + "(לחץ ליציאה)";
+            }
+            else
+            {
+                LoginLogout.HRef = "Login.aspx";
+                LoginLogout.InnerText = "כניסה";
+            }
+
+        }
+    }
+    private string GetImagePathForDay(string dayOfWeek)
+    {
+        string path = "images/week/";
+        // Use different image paths for each day
+        switch (dayOfWeek)
+        {
+            case "Sunday":
+                return path + "sunday.png";
+            case "Monday":
+                return path + "monday.png";
+            case "Tuesday":
+                return path + "tuesday.png";
+            case "Wednesday":
+                return path + "wednesday.png";
+            case "Thursday":
+                return path + "thursday.png";
+            case "Friday":
+                return path + "friday.png";
+            case "Saturday":
+                return path + "saturday.png";
+            default:
+                return path + "default.png"; // Fallback image
+        }
     }
 }
